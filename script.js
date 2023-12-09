cb7539d1451511bc3b34149b7552d8786b0a9026
Joseph Venturi <josephventuri@gmail.com>
2023-12-09 11:09:15 -0800

    updated javascript code

--- script.js
+++ script.js
@@ -5,31 +5,59 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    //Convert playerSelection to lowercase for case insensitivity
     playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
-    //Define the rules of the game
     const rules = {
         rock: 'scissors',
         paper: 'rock',
         scissors: 'paper'
     };
 
-    //check for a tie
     if (playerSelection === computerSelection) {
         return "It's a tie! Play again.";
     }
 
-    //Determine the winner
     if (rules[playerSelection] === computerSelection) {
-        return `You Win! ${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(computerSelection)}` ;
+        return `You Win! ${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(computerSelection)}`;
     } else {
-        return `You Lose! ${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(playerSelection)}` ;
-
+        return `You Lose! ${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(playerSelection)}`;
     }
 }
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Choose Rock, Paper or Scissors:");
+        const computerSelection = getComputerChoice();
+        console.log(`Round ${i + 1}, Player: ${capitalizeFirstLetter(playerSelection)}, Computer: ${capitalizeFirstLetter(computerSelection)}`);
+        
+        let result = playRound(playerSelection, computerSelection);
+        console.log(result);
+        
+        if (result.startsWith("You Win")) {
+            playerScore++;
+        } else if (result.startsWith("You Lose")) {
+            computerScore++;
+        }
+        // No score update if it's a tie
+    }
+
+    if (playerScore > computerScore) {
+        console.log(`You won the game! Final Score: Player ${playerScore} - Computer ${computerScore}`);
+    } else if (computerScore > playerScore) {
+        console.log(`You lost the game! Final Score: Player ${playerScore} - Computer ${computerScore}`);
+    } else {
+        console.log(`The game is a tie! Final Score: Player ${playerScore} - Computer ${computerScore}`);
+    }
+}
+
+game(); // Start the game
+
 
